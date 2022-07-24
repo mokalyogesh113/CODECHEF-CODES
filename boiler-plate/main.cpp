5a925f90e9cb26df729c930be9592dc251b1ef5d
Yogesh Mokal <mokalyogesh113@gmail.com>
2022-07-24 19:37:26 +0530

    Create main.cpp

---
+++ boiler-plate/main.cpp
@@ -0,0 +1,29 @@
+//Code By @trickymindme
+#include<bits/stdc++.h>
+#define ll long long int 
+#define vi vector<int>
+#define vll vector<long long int>
+#define all(c) (c).begin(),(c).end()
+using namespace std;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+     ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);s
+    int T=1;  
+    cin>>T;
+    for(int c=1;c<T+1; c++)
+    {
+        // cout<<"Case #"<<c<<": ";
+        solve();
+    }
+
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;    
+}
+
