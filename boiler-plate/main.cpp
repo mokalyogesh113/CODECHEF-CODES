99f720ddc180ae821a56597bc3c09d1c00e5266a
Yogesh Mokal <mokalyogesh113@gmail.com>
2023-10-31 22:43:39 +0530

    Did Changes in boiler-plate

--- boiler-plate/main.cpp
+++ boiler-plate/main.cpp
@@ -3,9 +3,38 @@
 #define ll long long int 
 #define vi vector<int>
 #define vll vector<long long int>
+#define vvi vector<vi>
+#define vvl vector<vll>
+#define vs vector<string>
+#define  pii pair<int,int>
+#define mpii map<int,int>
+#define mem(a,b) memset(a,(b),sizeof(a))
+#define FOR(i,j,k,in) for(int i=j;i<k;i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i,j) FOR(i,0,j,1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
 #define all(c) (c).begin(),(c).end()
 #define scanArray(v) for(auto &i:v) cin>>i;
 #define printArray(v) for(auto &i:v) cout<<i<<" "; cout<<endl;
+#define MP make_pair
+#define pb push_back
+#define eb emplace_back
+#define INF (int)1e9
+#define MOD 1000000007
+#define MOD1 998244353
+#define PI 3.141592653589793238462
+
+#define _test() int _T;  cin>>_T; while(_T--)
+#define _test2() for(int c=1;c<T+1; c++){  cout<<"Case #"<<c<<": "; solve();}
+#define _print_time() cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC  << " mS\n"
+
+// Functions
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+
 using namespace std;
 
 void solve()
@@ -15,17 +44,14 @@ void solve()
 
 int main()
 {
-     ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
-    int T=1; $1  
-    cin>>T;
-    for(int c=1;c<T+1; c++)
-    {
-        // cout<<"Case #"<<c<<": ";
+    _test() $1
         solve();
-    }
 
-    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC  << " mS\n";
+
+    _print_time();
+    
     return 0;    
 }
 
