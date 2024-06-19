627f70c2f98f831354e7c7ab9cb54e799fee0175
Yogesh Mokal <mokalyogesh113@gmail.com>
2024-06-19 19:46:14 +0530

    Update main.cpp

--- boiler-plate/main.cpp
+++ boiler-plate/main.cpp
@@ -31,8 +31,6 @@
 // Functions
 ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
 ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
-string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
-string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
 
 using namespace std;
